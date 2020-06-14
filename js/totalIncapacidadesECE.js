1b8336cfba62c00b3b6c479c8f1fb2e422d17f47
rlemuxh <rlemush@yahoo.com.mx>
2020-06-14 14:14:34 -0500

    Create totalIncapacidadesECE.js
    Se crea script para grafica de porcentaje de incapacidades emitidas por ECE

---
+++ js/totalIncapacidadesECE.js
@@ -0,0 +1,12 @@
+function showChartTotIncapacidadesECE(data) {
+    //console.log(data);
+
+    var datosTotIncapacidadesECE = [
+        { label: data[0], data: data[1], lines: { show: true, fill: false, lineWidth: 5 }, points: {show: true, radius: 5} },
+        { label: data[2], data: data[3], lines: { show: true, fill: false, lineWidth: 5 }, points: {show: true, radius: 5} },
+        { label: data[4], data: data[5], lines: { show: true, fill: false, lineWidth: 5 }, points: {show: true, radius: 5} }
+    ];
+
+    var legendContIncapacidadesECE = document.getElementById("legendTotIncapacidadesECE");
+    $.plot("#tot-incapacidades-ece", datosTotIncapacidadesECE, {legend: { show: true, position: "se", noColumns: 1, container: legendContIncapacidadesECE }, series: {lines: {show: false } }, grid: { hoverable: true, clickable: true }, zoom: {interactive: true}, pan: {interactive: true, enableTouch: true} });
+}
