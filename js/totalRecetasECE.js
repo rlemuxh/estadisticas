3725fa79cbb56c9e767ee9151af5d126d2a45a86
rlemuxh <rlemush@yahoo.com.mx>
2020-06-14 13:59:07 -0500

    Recetas ECE Js
    Se agrega el script para poder realizar la graficación de los datos de emisión de recetas de ECE

---
+++ js/totalRecetasECE.js
@@ -0,0 +1,12 @@
+function showChartTotRecetasECE(data) {
+    //console.log(data);
+
+    var datosTotRecetasECE = [
+        { label: data[0], data: data[1], lines: { show: true, fill: false, lineWidth: 5 }, points: {show: true, radius: 5} },
+        { label: data[2], data: data[3], lines: { show: true, fill: false, lineWidth: 5 }, points: {show: true, radius: 5} },
+        { label: data[4], data: data[5], lines: { show: true, fill: false, lineWidth: 5 }, points: {show: true, radius: 5} }
+    ];
+
+    var legendContRecetasECE = document.getElementById("legendTotRecetasECE");
+    $.plot("#tot-recetas-ece", datosTotRecetasECE, {legend: { show: true, position: "se", noColumns: 1, container: legendContRecetasECE }, series: {lines: {show: false } }, grid: { hoverable: true, clickable: true }, zoom: {interactive: true}, pan: {interactive: true, enableTouch: true} });
+}
