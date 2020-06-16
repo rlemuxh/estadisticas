a42c75e8176b27950967df15fa4d951f9cfe9526
rlemuxh <rlemush@yahoo.com.mx>
2020-06-16 07:46:33 -0500

    Update ConfigBD.php
    Data base info connwction

--- class/ConfigBD.php
+++ class/ConfigBD.php
@@ -1,6 +1,6 @@
 <?php
 define('DB_HOST', 'localhost');
-define('DB_USER', 'soporte');
-define('DB_PASS', 'deviltaz');
-define('DB_NAME', 'est_simf');
+define('DB_USER', 'myuser');
+define('DB_PASS', 'mypass');
+define('DB_NAME', 'mybd');
 define('DB_CHARSET', 'utf-8');
